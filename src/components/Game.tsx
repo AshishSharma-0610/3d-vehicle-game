6a4d2953938bb033e8876e99218faf9d49bf6965
AshishSharma-0610 <136923510+AshishSharma-0610@users.noreply.github.com>
2024-09-09 21:07:00 +0530

    Update Game.tsx

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -9,6 +9,7 @@ import GameOver from './GameOver'
 
 function Scene({ onGameOver }: { onGameOver: () => void }) {
     const vehicleRef = useRef<any>(null)
+    const orbitControlsRef = useRef<any>(null)
     const { camera } = useThree()
     const keysPressed = useRef<{ [key: string]: boolean }>({})
 
@@ -41,15 +42,14 @@ function Scene({ onGameOver }: { onGameOver: () => void }) {
             vehicleRef.current.steer(mouseX)
 
             const vehiclePosition = vehicleRef.current.getPosition()
-            camera.position.x = vehiclePosition[0]
-            camera.position.y = vehiclePosition[1] + 5
-            camera.position.z = vehiclePosition[2] + 10
-            camera.lookAt(vehiclePosition[0], vehiclePosition[1], vehiclePosition[2])
+            orbitControlsRef.current.target.set(vehiclePosition[0], vehiclePosition[1], vehiclePosition[2])
         }
     })
 
     return (
         <>
+            <OrbitControls ref={orbitControlsRef} enablePan={false} enableZoom={false}
+                minDistance={10} maxDistance={10} />
             <ambientLight intensity={0.6} />
             <pointLight position={[10, 10, 10]} castShadow />
             <Physics gravity={[0, -9.81, 0]}>
@@ -94,8 +94,7 @@ export default function Game() {
             <Canvas shadows key={gameKey}>
                 <color attach="background" args={['#87CEEB']} />
                 <fog attach="fog" args={['#87CEEB', 0, 100]} />
-                <PerspectiveCamera makeDefault position={[0, 5, 10]} />
-                <OrbitControls enabled={false} />
+                <PerspectiveCamera makeDefault position={[0, 10, 20]} />
                 <Scene onGameOver={handleGameOver} />
             </Canvas>
             <div className="absolute top-0 left-0 m-4 text-white text-2xl font-bold bg-black bg-opacity-50 p-2 rounded">
